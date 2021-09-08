204cc6aff6c18a2fde372185fdef135315c4d3e8
Toanhn <80703338+nhattoan009@users.noreply.github.com>
2021-09-08 22:19:47 +0700

    hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
+  // imports: [
+  //   BrowserModule,
+  //   AppRoutingModule
+  // ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
